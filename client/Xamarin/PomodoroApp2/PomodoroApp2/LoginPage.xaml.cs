8c862ccf52eea7dda6ee49b6defb9fba0db567c8
Marcell Spies <marcells@gmx.de>
2018-03-02 11:23:32 +0100

    Added xamarin client.

---
+++ client/Xamarin/PomodoroApp2/PomodoroApp2/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PomodoroApp2
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginPage : ContentPage
+	{
+		public LoginPage ()
+		{
+			InitializeComponent ();
+		}
+
+	    private async void OnLogin (object sender, EventArgs e)
+	    {
+	        var page = new MainPage {Name = nameText.Text};
+	        await Navigation.PushAsync (page);
+	    } 
+	}
+}
---
+++ client/Xamarin/PomodoroApp2/PomodoroApp2/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace PomodoroApp2
+{
+	public partial class MainPage : ContentPage
+	{
+        private readonly PomodoroAPI _api = new PomodoroAPI();
+
+	    public MainPage ()
+	    {
+	        InitializeComponent ();
+            
+            pomodoroList.ItemTapped += PomodoroList_ItemTapped;
+	    }
+
+	    public string Name { get; set; }
+
+	    private void PomodoroList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Name = ((PomodoroAPI.PomodoroEntry) e.Item).Name;
+            nameText.Text = Name;
+        }
+
+        protected override async void OnAppearing()
+	    {
+	        base.OnAppearing();
+
+	        await _api.StartPomodoroAsync(Name);
+	        pomodoroList.ItemsSource = await _api.GetPomodorosAsync();
+	    }
+
+	    public void RefreshPomodoros()
+	    {
+	        var entries = _api.GetPomodoros();
+            
+	        pomodoroList.ItemsSource = entries;
+	    }
+	    private void OnRefresh (object sender, EventArgs e)
+	    {
+	        RefreshPomodoros();
+            pomodoroList.EndRefresh();
+	    } 
+	    
+	    private void OnStart (object sender, EventArgs e)
+	    {
+	        _api.StartPomodoro(Name);
+            RefreshPomodoros();
+	    } 
+	    
+	    private void OnStop (object sender, EventArgs e)
+	    {
+	        _api.StopPomodoro(Name);
+            RefreshPomodoros();
+	    }
+
+	    private void OnTextChanged(object sender, TextChangedEventArgs e)
+	    {
+	        Name = nameText.Text;
+	    }
+	}
+}
