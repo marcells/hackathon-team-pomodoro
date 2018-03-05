c14a9c596f0d13a92c8a7ae102d74d96246f1d31
Ralf Kornelius <r.kornelius@vescon.com>
2018-03-05 09:51:39 +0100

    added PomodoroBridgeClient

---
+++ client/PomodoroBridgeClient/PomodoroBridgeClient/Api.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Pomodoro
+{
+    public class Api
+    {
+        private readonly IWebRequester _webRequester;
+
+        public string BaseUrl { get; set; } = @"http://vc024.vescon.com:3000/api/";
+        ////public string BaseUrl { get; set; } = @"https://team-pomodoro.azurewebsites.net/api/";
+
+        public bool Debug { get; set; }
+
+        public Api(IWebRequester webRequester)
+        {
+            _webRequester = webRequester;
+        }
+
+        public static string Humanize(TimeSpan timeSpan)
+        {
+            double days = timeSpan.Days;
+            var hours = timeSpan.Hours + days * 24;
+            var minutes = timeSpan.Minutes + hours * 60;
+            if (minutes <= 1)
+                return "Just Now";
+
+            var years = Math.Floor(timeSpan.TotalDays / 365);
+            if (years >= 1)
+                return $"{years} year{(years >= 2 ? "s" : null)} ago";
+
+            var weeks = Math.Floor(timeSpan.TotalDays / 7);
+            var result = string.Empty;
+            if (weeks >= 1)
+            {
+                var partOfWeek = days - weeks * 7;
+                if (partOfWeek > 0)
+                    result = $", {partOfWeek} day{(partOfWeek > 1 ? "s" : null)}";
+
+                return $"{weeks} week{(weeks >= 2 ? "s" : null)}{result} ago";
+            }
+
+            if (days >= 1)
+            {
+                var partOfDay = hours - days * 24;
+                if (partOfDay > 0)
+                    result = $", {partOfDay} hour{(partOfDay > 1 ? "s" : null)}";
+
+                return $"{days} day{(days >= 2 ? "s" : null)}{result} ago";
+            }
+
+            if (hours >= 1)
+            {
+                var partOfHour = minutes - hours * 60;
+                if (partOfHour > 0)
+                    result = $", {partOfHour} minute{(partOfHour > 1 ? "s" : null)}";
+
+                return $"{hours} hour{(hours >= 2 ? "s" : null)}{result} ago";
+            }
+
+            // Only condition left is minutes > 1
+            return minutes.ToString("{0} minutes ago");
+        }
+
+        public void Stop(string name)
+        {
+            var requestBody = new PutName {name = name};
+            var json = JsonConvert.SerializeObject(requestBody);
+            ExecuteRequest("pomodoro", "DELETE", json);
+        }
+
+        public void Start(string name)
+        {
+            var requestBody = new PutName {name = name};
+            var json = JsonConvert.SerializeObject(requestBody);
+            ExecuteRequest("pomodoro", "PUT", json);
+        }
+
+        public List<PomodoroEntry> GetAll()
+        {
+            var response = ExecuteRequest("pomodoros", "GET");
+            var entries = JsonConvert.DeserializeObject<List<PomodoroEntry>>(response);
+            return entries;
+        }
+
+        public void Clear()
+        {
+            var entries = GetAll();
+            foreach (var entry in entries)
+                Stop(entry.Name);
+        }
+
+        private string ExecuteRequest(string requestName, string requestMethod, string body = null)
+        {
+            return _webRequester.ExecuteRequest(BaseUrl + requestName, requestMethod, body);
+        }
+
+        public interface IWebRequester
+        {
+            string ExecuteRequest(string url, string method, string body = null);
+        }
+
+        public class PomodoroEntry
+        {
+            public string Name { get; set; }
+            public DateTime Time { get; set; }
+        }
+
+        private class PutName
+        {
+            public string name { get; set; }
+        }
+    }
+}
---
+++ client/PomodoroBridgeClient/PomodoroBridgeClient/PomodoroClient.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bridge.Html5;
+using Pomodoro;
+
+namespace PomodoroBridgeClient
+{
+    class PomodoroClient
+    {
+        private const bool Debug = true;
+        private readonly Api _api = new Api(new BridgeNetWebRequester {Logger = Dump});
+
+        private HTMLInputElement _textBoxName;
+        private HTMLTableElement _table;
+
+        public void Render()
+        {
+            var labelWelcome = new HTMLLabelElement
+            {
+                InnerHTML = "Welcome to Pomodoro!",
+                Style =
+                {
+                    FontSize = "32px",
+                    Margin = "10px"
+                }
+            };
+
+            _textBoxName = new HTMLInputElement
+            {
+                Placeholder = "Enter name…",
+                Value = "PomodoroBridgeClient"
+            };
+
+            _table = new HTMLTableElement();
+            var buttonGetAll = new HTMLButtonElement
+            {
+                InnerHTML = "Get all",
+                OnClick = ev => RefreshList()
+            };
+
+            var buttonStart = new HTMLButtonElement
+            {
+                InnerHTML = "Start",
+                OnClick = ev =>
+                {
+                    _api.Start(_textBoxName.Value);
+                    RefreshList();
+                }
+            };
+            var buttonStop = new HTMLButtonElement
+            {
+                InnerHTML = "Stop",
+                OnClick = ev =>
+                {
+                    _api.Stop(_textBoxName.Value);
+                    RefreshList();
+                }
+            };
+
+            // Add to the document body
+            var htmlBodyElement = Document.Body;
+            htmlBodyElement.AppendChild(labelWelcome);
+            htmlBodyElement.AppendChild(new HTMLBRElement());
+
+            htmlBodyElement.AppendChild(_textBoxName);
+            htmlBodyElement.AppendChild(new HTMLBRElement());
+
+            htmlBodyElement.AppendChild(buttonGetAll);
+            htmlBodyElement.AppendChild(buttonStart);
+            htmlBodyElement.AppendChild(buttonStop);
+            htmlBodyElement.AppendChild(new HTMLBRElement());
+
+            htmlBodyElement.AppendChild(_table);
+        }
+
+        private void RefreshList()
+        {
+            var entries = _api.GetAll();
+            FillEntries(entries);
+        }
+
+        private void FillEntries(IEnumerable<Api.PomodoroEntry> entries)
+        {
+            // clear
+            foreach (var row in _table.Rows.ToList())
+                _table.DeleteRow(row.RowIndex);
+
+            var head = _table.CreateTHead();
+            var headRow = head.InsertRow();
+            var headCell1 = headRow.InsertCell();
+            headCell1.AppendChild(new HTMLLabelElement {InnerHTML = "Name"});
+            var headCell2 = headRow.InsertCell();
+            headCell2.AppendChild(new HTMLLabelElement {InnerHTML = "Time"});
+
+            var now = DateTime.Now;
+            foreach (var entry in entries)
+            {
+                var labelName = new HTMLLabelElement
+                {
+                    InnerHTML = entry.Name,
+                    OnClick = mouseEvent => _textBoxName.Value = mouseEvent.CurrentTarget.InnerHTML
+                };
+
+                var elapsed = now - entry.Time;
+                var time = Api.Humanize(elapsed);
+                var labelTime = new HTMLLabelElement
+                {
+                    InnerHTML = time
+                };
+
+                var row = _table.InsertRow();
+                var cell1 = row.InsertCell();
+                cell1.AppendChild(labelName);
+                var cell2 = row.InsertCell();
+                cell2.AppendChild(labelTime);
+            }
+        }
+
+        private static void Dump(string msg)
+        {
+            if (Debug)
+                Console.WriteLine(msg);
+        }
+    }
+}
